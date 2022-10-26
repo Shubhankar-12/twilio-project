ec05dc6908c2dc1f92967dbdb320ccf7f58934e0
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-10-26 21:45:38 +0530

    final push

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// all required files
 require("dotenv").config();
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
@@ -8,17 +9,20 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// rendering the dashboard html to receive the message body and media url
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/dashboard.html`, (err) => {
     if (err) console.log(err);
   });
 });
+
+// Getting the message body and media url and sending to twilio registered whatsapp number
 app.post("/", (req, res) => {
   client.messages
     .create({
       body: req.body.message,
       from: "whatsapp:+14155238886",
-      to: "whatsapp:" + req.body.contactNo,
+      to: "whatsapp:" + process.env.PHONE,
       mediaUrl: req.body.url,
     })
     .then((message) => console.log(message.sid))
@@ -26,6 +30,7 @@ app.post("/", (req, res) => {
   res.redirect("/");
 });
 
+// Started Listening to the server
 app.listen(3000, () => {
   console.log("Server has been started listening at 3000...");
 });
